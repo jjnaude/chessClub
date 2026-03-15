b63e9bb6d217825e142fdec08d610591800d86c8
jjnaude <naude.jj@gmail.com>
2026-03-15 16:52:21 +0200

    Add GitHub Pages deployment for React app

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { registerSW } from 'virtual:pwa-register'
 import './index.css'
 import { AppLayout } from './layouts/AppLayout'
@@ -10,7 +10,7 @@ import { DashboardPage } from './pages/DashboardPage'
 import { LadderPage } from './pages/LadderPage'
 import { LoginPage } from './pages/LoginPage'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <AppLayout />,
